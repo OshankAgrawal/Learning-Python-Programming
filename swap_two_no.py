57923122ee1003d933c53ca206b9a5370128179b
OshankAgrawal <oshankagrawal@gmail.com>
2025-01-02 03:24:23 +0530

    Swap of two numbers

---
+++ swap_two_no.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the value of first number."))
+num2 = int(input("Enter the value of second number."))
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+print("After swaping the values\nValue of first number is ",num1,"\nValues of second number is ",num2)
